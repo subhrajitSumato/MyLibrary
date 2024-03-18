7cd0f7bcca7287138137ba038422ed4ce9aa76fd
subhrajitdeb <subhrajitdeb54@gmail.com>
2024-03-18 15:23:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mylibrary/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.mylibrary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.image_preview.ImagePreview
+import com.example.mylibrary.ui.theme.MyLibraryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyLibraryTheme {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ){
+                    ImagePreview(
+                        image = painterResource(id = R.drawable.iconn),
+                        description = "My Library",
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .size(150.dp)
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ image-preview/src/main/java/com/example/image_preview/ImagePreview.kt
@@ -0,0 +1,68 @@
+package com.example.image_preview
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ImagePreview(
+    image: Painter,
+    modifier: Modifier = Modifier,
+    description: String = "",
+    contentDescription: String = "",
+    onImageClick: () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .shadow(15.dp, RoundedCornerShape(15.dp))
+            .clickable { onImageClick() }
+    ) {
+        Image(
+            painter = image,
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodySmall,
+            color = Color.White,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    Brush.verticalGradient(
+                        listOf(
+                            Color.Transparent,
+                            Color.Black
+                        )
+                    )
+                )
+                .align(Alignment.BottomStart)
+                .padding(8.dp)
+        )
+    }
+}
